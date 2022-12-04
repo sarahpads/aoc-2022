8bdd1a0a006e5be889a3f1fdc9232cae9ff1d4a7
Sarah Padovani <sarah.padovani@redspace.com>
2022-12-04 11:32:05 -0400

    day 4

---
+++ day-4/main-2.py
@@ -0,0 +1,21 @@
+import sys
+
+count = 0
+
+with open("input.txt") as input:
+  for line in input:
+    # use list comprehension to strip newlines and map each line into int arrays
+    # use unpacking to assign each int pair to an elf
+    elf1, elf2 = [list(map(int, elf.split('-'))) for elf in line.rstrip().split(',')]
+    # construct a range out of the min/max values of each elf to find their overlap
+    # if the highest start and lowest end create a valid range, we know they overlap
+    # we need to add +1 to the min to make sure overlaps of a single number are counted
+    # (range(7,7) has a length of 0, but in our case this counts as 1)
+    overlap = len(range(max(elf1[0], elf2[0]), min(elf1[-1], elf2[-1]) + 1))
+
+    if (overlap > 0):
+      count += 1
+
+print('count', count)
+
+sys.exit()
